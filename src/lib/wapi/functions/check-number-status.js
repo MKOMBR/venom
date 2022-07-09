dfb058685b01ad1c792b94a7cf3bfc07218ce3fd
Julio Vaz <jmatheus.vaz@gmail.com>
2022-07-09 10:39:40 -0300

    new fix https://github.com/orkestral/venom/pull/1794/commits/c7ef7f10b1ff53818923893698e4681e6d34bedd

--- src/lib/wapi/functions/check-number-status.js
+++ src/lib/wapi/functions/check-number-status.js
@@ -30,7 +30,7 @@ export async function checkNumberStatus(id, conn = false) {
 
     if (WAPI.isBeta()) {
       return await Store.checkNumberBeta
-        .queryPhoneExists(id)
+        .queryPhoneExists(new Store.WidFactory.createWid(id))
         .then((result) => {
           if (!!result && typeof result === 'object') {
             const data = {
